9413d5753c51ed2a279039d1fc8f93897f9eef54
Mārtiņš Tereško <martins.teresko@gmail.com>
2016-02-14 02:19:14 +0200

    implementing the changed depencencies

--- src/Fracture/Routing/Routable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fracture\Routing;
-
-interface Routable
-{
-    public function getUri();
-    public function setParameters(array $parameters);
-}
--- src/Fracture/Routing/Router.php
+++ src/Fracture/Routing/Router.php
@@ -2,6 +2,8 @@
 
 namespace Fracture\Routing;
 
+use Fracture\Http\Routable;
+
 class Router
 {
 
